688ec26127f3695a9a9a416cafed1ea406083e51
Kiara Ruiz <23100833@ue.edu.pe>
2026-04-02 20:53:51 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Web lista");
+const toggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
