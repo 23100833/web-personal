a16481c386f60a6fbc863e21ea1940ca554f8e80
Kiara Ruiz <23100833@ue.edu.pe>
2026-04-02 21:17:46 -0500

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const navLinks = document.querySelector(".nav-links");
 toggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 300;
+
+        if (top > offset) {
+            sec.classList.add("visible");
+        }
+    });
+});
